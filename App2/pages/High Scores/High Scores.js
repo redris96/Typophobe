41bd053b3df4e66b46d6285f87cdd9390e088b41
mukulhase <mukulhase@gmail.com>
2015-03-29 11:50:08 +0530

    Fix shit high

--- App2/pages/High Scores/High Scores.js
+++ App2/pages/High Scores/High Scores.js
@@ -20,6 +20,6 @@
         }
     });
 })();
-function load() {
+function loadx() {
     document.getElementById("high").innerHTML = "" + "<div  class=\" first\" >1:" + "\t\t\t" + parseInt(localStorage.getItem("top1")) + "</div> <div  class=\" second\" >2: " + "\t\t\t" + parseInt(localStorage.getItem("top2")) + "</div> <div   class=\" third\" >3: " + "\t\t\t" + parseInt(localStorage.getItem("top3")) + "</div>";
 }
