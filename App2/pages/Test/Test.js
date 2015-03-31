208974babca399808cf3bc04262c7f4b95de6ae4
babarishith <babarishith@gmail.com>
2015-03-31 09:53:10 +0530

    idk

--- App2/pages/Test/Test.js
+++ App2/pages/Test/Test.js
@@ -171,27 +171,6 @@ function randomize() {
     document.getElementById("typeit").disabled = false;
 
 }
-function reset() {
-    /*var msg = new Windows.UI.Popups.MessageDialog("WPM: " + wc + "\nAccuracy: " + (Number((gc / i) * 100).toPrecision(4)).toString() + "%");
-    msg.commands.append(new Windows.UI.Popups.UICommand(
-            "Try again",));
-    msg.commands.append(
-        new Windows.UI.Popups.UICommand("Back", commandInvokedHandler));
-    msg.showAsync();*/
-    document.getElementById("typeit").value = "";
-    i = 0;
-    flag = 1; wh = 94;
-    gc = 0; pr = 0;
-    c, wc = 0; state = 0; up();
-    document.getElementById("dis").innerHTML = '<span style="background-color: #9c8c8c">' + legit.substr(0, tp) + '</span>' + legit.substr(tp);
-    document.getElementById("wc").innerHTML = "WPM: 0";
-    document.getElementById("acc").innerHTML = "Accuracy: 0%";
-    clearInterval(ticker);
-    document.getElementById("countdown").innerHTML = "Countdown: 60";
-    document.getElementById("typeit").focus();
-    document.getElementById("typeit").disabled = false;
-    
-}
 
 var timeInSecs;
 var ticker;
@@ -268,14 +247,5 @@ function up(){
     $("#dis").animate({ scrollTop: 0 }, "fast");
 }
 
-/*function load() {
-    Windows.Storage.StorageFile.getFileFromApplicationUriAsync("ms-appx:///pages//pages/Text/c/c1.txt").done(
-    function (file) {
-        // Process file
-        legit = file.toString;
-        reset();
-    }
-);
-}*/
 
 
