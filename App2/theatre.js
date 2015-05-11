0ae48f4df192fd40a29d2dd0a59356de3dd343fd
mukulhase <mukulhase@gmail.com>
2015-05-11 12:58:05 +0530

    pimped out title

--- App2/theatre.js
+++ App2/theatre.js
@@ -1,35 +1,4 @@
-﻿(function () {
-    "use strict";
-
-    WinJS.UI.Pages.define("/pages/home/home.html", {
-        // This function is called whenever a user navigates to this page. It
-        // populates the page elements with the app's data.
-        ready: function (element, options) {
-            // TODO: Initialize the page here.
-            WinJS.Utilities.query("a").listen("click", 
-                this.linkClickEventHandler, false);
-            console.log("run");
-            var theater = new TheaterJS()
-            theater.describe("Typophobe", { speed: .8, accuracy: .4, invincibility: 0.5 }, "#typophobe");
-            var i = 0;
-            while (i < 1000) {
-                theater.write("Typophobe:Typophobe", 400);
-                i++;
-                console.log("run");
-            }
-        },
-        linkClickEventHandler: function (eventInfo) {
-        eventInfo.preventDefault();
-        var link = eventInfo.currentTarget;
-        WinJS.Navigation.navigate(link.href);
-       
-        }
-    }
-    );
-
-})();
-
-/*
+﻿/*
  * The MIT License (MIT)
  *
  * Copyright (c) 2015 Gabin Aureche <hello@gabinaureche.com>
